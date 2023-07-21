eb2294b4a19453f863cce9e2e789f313bea319ff
ykji <yvarshney44@gmail.com>
2023-07-21 12:36:34 +0530

    exit enter animation

--- src/components/ExitEnterAnimation.jsx
+++ src/components/ExitEnterAnimation.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+import React, { useState } from "react";
 
 const ExitEnterAnimation = () => {
+  const [show, setShow] = useState(true);
   return (
-    <div>ExitEnterAnimation</div>
-  )
-}
+    <div className="exitEnterContainer">
+      <h1>Exit Enter Animation</h1>
 
-export default ExitEnterAnimation
+      <AnimatePresence>
+        {show && (
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 100 }}
+            transition={{ duration: 1 }}
+          ></motion.div>
+        )}
+      </AnimatePresence>
+      <button onClick={() => setShow(!show)}>{show ? "Remove" : "Add"}</button>
+    </div>
+  );
+};
+
+export default ExitEnterAnimation;
